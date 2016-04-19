2e6a2fd8139b0fcfcf57a0de8a525e243e3f3b03
Clean14.04.4_SARA <chinasaz@yandex.com>
2016-04-19 10:44:47 +0200

    Takes user input

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ app.controller('MainCtrl', [
                 
     function($scope){
         $scope.test = 'Hello world!';
+        // Data for the view
         $scope.posts = [
-            'post 1',
-            'post 2',
-            'post 3',
-            'post 4',
-            'post 5'
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+            {title: 'post 3', upvotes: 15},
+            {title: 'post 4', upvotes: 9},
+            {title: 'post 5', upvotes: 4}
         ];
+        // Functions to be implemented in the view
+        $scope.addPost = function(){
+        	if($scope.title && !($scope.title === '') ) { 
+            	$scope.posts.push({title: $scope.title, upvotes: 0});
+            	$scope.title = '';
+            }
+        };
 
 }]);
